3a5041c8b22d94f88ab6f2cf5f8118a1f3ba4c7d
Sergio Prolo <smauricioj@gmail.com>
2025-08-25 10:04:18 -0300

    Ajeitando em classes

---
+++ app/src/main/java/ifsc/poo/listaUm/Exercicio1.java
@@ -0,0 +1,17 @@
+package ifsc.poo.listaUm;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a, b, c;
+        System.out.print("Digite o valor de a: ");
+        a = sc.nextFloat();
+        System.out.print("Digite o valor de b: ");
+        b = sc.nextFloat();
+        System.out.print("Digite o valor de c: ");
+        c = sc.nextFloat();
+        System.out.printf("O valor do discriminante é %.2f\n", (b*b)-(4*a*c));
+    }
+}
---
+++ app/src/main/java/ifsc/poo/listaUm/Exercicio9.java
@@ -0,0 +1,22 @@
+package ifsc.poo.listaUm;
+
+public class Exercicio9 {
+    public static void main(String[] args) {
+        byte tam = 10;
+        int[][] mat = new int[tam][tam];
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0; j < tam; j++) {
+                if (i < j) {
+                    mat[i][j] = (2 * i) + (7 * j) - 2;
+                }
+                else if (i == j) {
+                    mat[i][j] = (3 * i * i) - 1;
+                } else {
+                    mat[i][j] = (4 * i * i * i) - (5 * j * j);
+                }
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
