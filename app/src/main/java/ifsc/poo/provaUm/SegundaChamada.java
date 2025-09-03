01ab39978203d4484014710ded17b7ccdf3e5c3d
Sergio Prolo <smauricioj@gmail.com>
2025-09-03 12:25:58 -0300

    Pequenos ajustes de código

--- app/src/main/java/ifsc/poo/provaUm/SegundaChamada.java
+++ app/src/main/java/ifsc/poo/provaUm/SegundaChamada.java
@@ -18,32 +18,31 @@ public class SegundaChamada {
         double RAIO_TERRA = 6367.4445;
         int N_CIDADES = 234, COL_NOME = 1, COL_LAT = 2, COL_LON = 3;
         Scanner sc = new Scanner(System.in);
+        sc.nextLine(); // remove linha do cabeçalho
         String[] nomes = new String[N_CIDADES];
         double[][] coord = new double[N_CIDADES][2];
-        sc.nextLine(); // remove linha do cabeçalho
         for (int i = 0; i < N_CIDADES && sc.hasNext(); i++) {
-            String linha = sc.nextLine();
-            String[] partes = linha.split(",");
+            String[] partes = sc.nextLine().split(",");
             nomes[i] = String.valueOf(partes[COL_NOME]);
             coord[i] = new double[]{
                     grau_para_rad(Double.parseDouble(partes[COL_LAT])),
                     grau_para_rad(Double.parseDouble(partes[COL_LON]))
             };
         }
-        double[][] dist = new double[N_CIDADES][N_CIDADES];
-        double max_dist = Double.MIN_VALUE;
-        int[] max_dist_cidades_index = new int[]{-1, -1};
+        double dist, max_dist = Double.MIN_VALUE;
+        int cidade_i = -1, cidade_j = -1;
         for (int i = 0; i < N_CIDADES; i++) {
             for (int j = 0; j <= i; j++) {
-                dist[i][j] = haverseno(RAIO_TERRA, coord[i][0], coord[j][0], coord[i][1], coord[j][1]);
-                if (dist[i][j] > max_dist) {
-                    max_dist = dist[i][j];
-                    max_dist_cidades_index = new int[]{i, j};
+                dist = haverseno(RAIO_TERRA, coord[i][0], coord[j][0], coord[i][1], coord[j][1]);
+                if (dist > max_dist) {
+                    max_dist = dist;
+                    cidade_i = i;
+                    cidade_j = j;
                 }
             }
         }
         System.out.printf("As capitais mais distantes são: %s e %s, com distância de %.2f km\n",
-                nomes[max_dist_cidades_index[0]], nomes[max_dist_cidades_index[1]], max_dist);
+                nomes[cidade_i], nomes[cidade_j], max_dist);
         sc.close();
     }
 }
