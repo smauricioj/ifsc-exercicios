6e9c88bb554fa604981059049374849cea92310b
Sergio Prolo <smauricioj@gmail.com>
2025-09-11 09:51:43 -0300

    Mais ajustes

--- app/src/main/java/ifsc/poo/provaDois/Hitbox.java
+++ app/src/main/java/ifsc/poo/provaDois/Hitbox.java
@@ -1,23 +1,18 @@
 package ifsc.poo.provaDois;
 import java.util.Objects;
+import java.util.Random;
 
 public class Hitbox {
     // Estáticos
     public static int contagem;
     public static final byte LARGURA_PADRAO, ALTURA_PADRAO;
-    public static final byte ORIGEM_X_PADRAO, ORIGEM_Y_PADRAO;
     public static final byte X_MIN, Y_MIN;
-    public static final byte REPR_LADO_MIN, REPR_LADO_MAX;
 
     static {
         LARGURA_PADRAO = 1;
         ALTURA_PADRAO = 1;
-        ORIGEM_X_PADRAO = 0;
-        ORIGEM_Y_PADRAO = 0;
         X_MIN = 0;
         Y_MIN = 0;
-        REPR_LADO_MAX = 20;
-        REPR_LADO_MIN = 3;
         contagem = 0;
     }
 
@@ -31,11 +26,9 @@ public class Hitbox {
     public Hitbox(double x, double y, double largura, double altura) {
         if (largura <= 0) largura = LARGURA_PADRAO;
         if (altura <= 0) altura = ALTURA_PADRAO;
-        if (x < X_MIN) x = X_MIN;
-        if (y < Y_MIN) y = Y_MIN;
 
-        this.x = x;
-        this.y = y;
+        this.setX(x);
+        this.setY(y);
         this.largura = largura;
         this.altura = altura;
         contagem++;
@@ -88,7 +81,6 @@ public class Hitbox {
     public static int getContagem() {
         return contagem;
     }
-
     public static void zeraContagem() {
         contagem = 0;
     }
@@ -99,9 +91,9 @@ public class Hitbox {
                 && Math.max(a.y, b.y) < Math.min(a.y+a.altura, b.y+b.altura);
         // Também é possível:
         // return a.x < b.x + b.largura &&
-        //       a.x + a.largura > b.x &&
-        //       a.y < b.y + b.altura &&
-        //       a.y + a.altura > b.y;
+        //        a.x + a.largura > b.x &&
+        //        a.y < b.y + b.altura &&
+        //        a.y + a.altura > b.y;
     }
 
     public double area() {
@@ -118,7 +110,26 @@ public class Hitbox {
     }
 
     public static void main(String[] args) {
-
+        Random r = new Random();
+        final int LARGURA_TELA = 50;
+        final int ALTURA_TELA = 30;
+        final int N_HITBOXES = 20;
+        Hitbox[] hitboxes = new Hitbox[N_HITBOXES];
+        for (int i = 0; i < N_HITBOXES; i++) {
+            double x = r.nextDouble(LARGURA_TELA);
+            double y = r.nextDouble(ALTURA_TELA);
+            double largura = r.nextDouble(LARGURA_TELA - x);
+            double altura = r.nextDouble(ALTURA_TELA - y);
+            hitboxes[i] = new Hitbox(x, y, largura, altura);
+        }
+        int contagem = 0;
+        for (int i = 0; i < N_HITBOXES; i++) {
+            for (int j = i+1; j < N_HITBOXES; j++) {
+                if (Hitbox.intersecta(hitboxes[i], hitboxes[j])) {
+                    contagem++;
+                }
+            }
+        }
+        System.out.println("Numero de pares que estão sobrepostos: "+contagem);
     }
 }
-
