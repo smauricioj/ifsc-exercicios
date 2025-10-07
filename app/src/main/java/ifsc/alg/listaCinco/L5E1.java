52c232c16dacd46fd7015c9757274b1503469780
Sergio Prolo <smauricioj@gmail.com>
2025-10-07 18:43:45 -0300

    Pequenas alterações

--- app/src/main/java/ifsc/alg/listaCinco/L5E1.java
+++ app/src/main/java/ifsc/alg/listaCinco/L5E1.java
@@ -10,8 +10,8 @@ public class L5E1 {
         // Início
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite o valor da temperatura em Fahrenheit: ");
-        tempF = sc.nextDouble(); // Leia tempF
-        tempC = (tempF - 32)*(5.0 / 9.0);
+        tempF = sc.nextDouble();            // 'Leia tempF'
+        tempC = (tempF - 32)*(5.0 / 9.0);   // Atenção para a divisão de doubles!
         System.out.printf("Temperatura em Celcius: %.3f%n", tempC);
         sc.close();
         // Fim
--- app/src/main/java/ifsc/alg/listaCinco/L5E4.java
+++ app/src/main/java/ifsc/alg/listaCinco/L5E4.java
@@ -7,11 +7,12 @@ public class L5E4 {
         // Variáveis
         int d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13;
         int segundaParte, terceiraParte;
-        int etapaA, etapaB;
+        int somaA, somaB;
 
         // Início
         Scanner sc = new Scanner(System.in);
 
+        // Entradas
         System.out.print("Entre com a primeira parte (1 dígito): ");
         d1 = sc.nextInt();
         System.out.print("Entre com a segunda parte (6 dígitos): ");
@@ -29,8 +30,7 @@ public class L5E4 {
         d4 = segundaParte % 10;
         segundaParte /= 10;
         d3 = segundaParte % 10;
-        segundaParte /= 10;
-        d2 = segundaParte;
+        d2 = segundaParte / 10;
 
         // Decifrando a terceira parte
         d12 = terceiraParte % 10;
@@ -40,13 +40,12 @@ public class L5E4 {
         d10 = terceiraParte % 10;
         terceiraParte /= 10;
         d9 = terceiraParte % 10;
-        terceiraParte /= 10;
-        d8 = terceiraParte;
+        d8 = terceiraParte / 10;
 
         // Algoritmo de verificação
-        etapaA = d1 + d3 + d5 + d7 + d9 + d11;
-        etapaB = d2 + d4 + d6 + d8 + d10 + d12;
-        d13 = 9 - ( ( (etapaA * 3) + etapaB - 1) % 10);
+        somaA = d1 + d3 + d5 + d7 + d9 + d11;
+        somaB = d2 + d4 + d6 + d8 + d10 + d12;
+        d13 = 9 - ((((somaB*3) + somaA) - 1) % 10);
         System.out.printf("Dígito verificador: %d%n", d13);
         sc.close();
         // Fim
